1a7ed7587af8bf83932b2785bfeeabb77664aaab
wael-chafei <waeltn7@gmail.com>
2022-12-12 22:02:33 +0100

    get user commit

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.example.demo.controller;
 import java.net.URI;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +17,8 @@ import com.example.demo.dao.EventRepo;
 import com.example.demo.dao.UserRepo;
 import com.example.demo.entities.Event;
 import com.example.demo.entities.User;
+import com.example.demo.service.EventService;
+import com.example.demo.service.UserService;
 
 
 @RestController
@@ -21,7 +26,7 @@ public class UserController {
  
 	@Autowired
 	UserRepo repo ; 
-	
+	@CrossOrigin(origins = "", allowedHeaders = "")
 	@PostMapping("add")
 	   public ResponseEntity<User> postBody(@RequestBody User user) {
 		   User persistedUser = repo.save(user);
@@ -37,6 +42,14 @@ public class UserController {
 		getted_user = repo.findById(id);
 		return getted_user;
 	}
+	
+	   @Autowired
+	   UserService userService;
+
+    @GetMapping("user")
+    public List<User> list() {
+        return userService.listAllUser();
+    }
 	
 
 
---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,21 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.dao.UserRepo;
+import com.example.demo.entities.User;
+
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
+public class UserService {
+    @Autowired
+    private UserRepo userRepository;
+    public List<User> listAllUser() {
+        return userRepository.findAll();
+    }
+}
